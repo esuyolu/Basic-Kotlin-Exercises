774dfbf5d58bd176dde0a26295aa35e0b0e32d5b
Esma Suyolu <esmasuyolu@gmail.com>
2020-10-04 15:39:44 +0300

    Add files via upload

---
+++ h.003.3-goldbach-theorem/README.md
@@ -0,0 +1,2 @@
+# h.003.3-goldbach-theorem
+ all even numbers larger than 2 can be written as the sum of two prime numbers (Goldbach's theorem). Write the program that takes an even number from the keyboard and prints it in the form of the sum of two prime numbers
---
+++ h.003.3-goldbach-theorem/src/org/esmasuyolu/goldbachTheorem.kt
@@ -0,0 +1,47 @@
+package org.esmasuyolu
+
+fun main()
+{
+    print("enter an even number: ")
+    val a = readLine()!!.toInt()
+
+    println("*********************")
+    goldbachTheorem(a)
+}
+
+fun goldbachTheorem(a: Int)
+{
+    for (i in 2..(a - 2)) {
+        if (isPrime(i) && isPrime(a - i))
+            println("$a = $i + ${(a - i)}")
+    }
+}
+
+fun isPrime(a: Int) : Boolean
+{
+    if (a <= 1)
+        return false
+
+    if (a % 2 == 0)
+        return a == 2
+
+    if (a % 3 == 0)
+        return a == 3
+
+    if (a % 5 == 0)
+        return a == 5
+
+    if (a % 7 == 0)
+        return a == 7
+
+    var i = 11
+
+    while (i * i <= a) {
+        if (a % i == 0)
+            return false
+
+        i += 2
+    }
+
+    return true
+}
